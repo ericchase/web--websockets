49f619a27b9eed4e0ddf0c54f85ed322f16251c2
ericchase <github@eric.addymail.com>
2024-04-07 16:08:59 -0500

    format, add port env
    Signed-off-by: ericchase <github@eric.addymail.com>

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 import uWS from 'uWebSockets.js';
 import { addWebSocket } from './websocket-factory.js';
-const port = 8080;
+const port = process.env.PORT || 3000;
 const app = uWS.App({});
 app.get('/*', async (res, req) => {
-    const url = req.getUrl();
-    const id = url.slice(url.lastIndexOf('/') + 1);
-    res.writeHeader('Access-Control-Allow-Origin', '*');
-    res.end(JSON.stringify(addWebSocket(app, id)));
+  const url = req.getUrl();
+  const id = url.slice(url.lastIndexOf('/') + 1);
+  res.writeHeader('Access-Control-Allow-Origin', '*');
+  res.end(JSON.stringify(addWebSocket(app, id)));
 });
 app.listen(port, (token) => {
-    if (token) {
-        console.log('URL:', `http://127.0.0.1:${port}/`);
-    }
-    else {
-        console.log('Failed to listen to port ' + port);
-    }
+  if (token) {
+    console.log('URL:', `http://127.0.0.1:${port}/`);
+  } else {
+    console.log('Failed to listen to port ' + port);
+  }
 });
--- websocket-factory.js
+++ websocket-factory.js
@@ -1,37 +1,36 @@
 import uWS from 'uWebSockets.js';
 const pathSet = new Set();
 export function addWebSocket(app, id) {
-    let created = false;
-    let ready = false;
-    try {
-        if (!pathSet.has(id)) {
-            console.log('websocket create /' + id);
-            pathSet.add(id);
-            app.ws('/' + id, {
-                compression: uWS.DISABLED,
-                maxPayloadLength: 1024,
-                idleTimeout: 10,
-                open: (ws) => {
-                    console.log('websocket connect /' + id);
-                    ws.subscribe('broadcast');
-                },
-                message: (ws, message, isBinary) => {
-                    console.log('websocket message /' + id);
-                    console.log(message);
-                    ws.publish('broadcast', message, isBinary);
-                },
-                drain: (ws) => {
-                    console.log('websocket backpressure /' + id);
-                    console.log('size ' + ws.getBufferedAmount());
-                },
-                close: (ws, code, message) => {
-                    console.log('websocket disconnect /' + id);
-                },
-            });
-            created = true;
-        }
-        ready = true;
+  let created = false;
+  let ready = false;
+  try {
+    if (!pathSet.has(id)) {
+      console.log('websocket create /' + id);
+      pathSet.add(id);
+      app.ws('/' + id, {
+        compression: uWS.DISABLED,
+        maxPayloadLength: 1024,
+        idleTimeout: 10,
+        open: (ws) => {
+          console.log('websocket connect /' + id);
+          ws.subscribe('broadcast');
+        },
+        message: (ws, message, isBinary) => {
+          console.log('websocket message /' + id);
+          console.log(message);
+          ws.publish('broadcast', message, isBinary);
+        },
+        drain: (ws) => {
+          console.log('websocket backpressure /' + id);
+          console.log('size ' + ws.getBufferedAmount());
+        },
+        close: (ws, code, message) => {
+          console.log('websocket disconnect /' + id);
+        },
+      });
+      created = true;
     }
-    catch (err) { }
-    return { created, ready };
+    ready = true;
+  } catch (err) {}
+  return { created, ready };
 }
