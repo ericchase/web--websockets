f955b8a7aab79aca357067f37a0ae5c90ca5be67
ericchase <github@eric.addymail.com>
2024-04-07 16:33:57 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import uWS from 'uWebSockets.js';
 import { addWebSocket } from './websocket-factory.js';
-const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 10000;
 const app = uWS.App({});
 app.get('/*', async (res, req) => {
   const url = req.getUrl();
@@ -8,10 +9,10 @@ app.get('/*', async (res, req) => {
   res.writeHeader('Access-Control-Allow-Origin', '*');
   res.end(JSON.stringify(addWebSocket(app, id)));
 });
-app.listen(port, (token) => {
+app.listen(host, port, (token) => {
   if (token) {
-    console.log('URL:', `http://127.0.0.1:${port}/`);
+    console.log('URL:', `http://${host}:${port}/`);
   } else {
-    console.log('Failed to listen to port ' + port);
+    console.log(`Failed to listen on http://${host}:${port}/`);
   }
 });
